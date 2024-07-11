41e0e2592fecda49d293673fa0d081f85bc8c66a
Daniel <vinojko@gmail.com>
2024-07-11 21:59:19 +0200

    Add background image to main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Header />
-        {children}
+        <Header /> {children}
       </body>
     </html>
   );
