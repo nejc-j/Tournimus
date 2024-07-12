d262e0772ca50690018b0201d3d7d9fd4032f82b
nejc-j <jj6493@student.uni-lj.si>
2024-07-12 19:44:06 +0200

    Add email verification

--- src/app/api/whoami/route.ts
+++ src/app/api/whoami/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { auth } from '@/auth';
 
-export const GET = auth(async ({ auth }) => {
-  return NextResponse.json({ user: auth?.user?.name });
+export const GET = auth(async ({ auth: authUser }) => {
+  return NextResponse.json({ user: authUser?.user?.name });
 });
