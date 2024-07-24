b8a161df49ac32a67a3d001a4e4e0ac239518fa2
nejc-j <jernej.jakop3@gmail.com>
2024-07-24 11:53:33 +0200

    Fix cards to center on small screens

--- src/components/TournamentList.tsx
+++ src/components/TournamentList.tsx
@@ -14,22 +14,25 @@ function TournamentList({ tournaments }: TournamentListProps) {
       ) : (
         <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           {tournaments?.map((tournament) => (
-            <TournamentCard
-              key={tournament.id}
-              id={tournament.id}
-              name={tournament.name}
-              startTime={tournament.startTime}
-              locationName={tournament.locationName}
-              street={tournament.street}
-              city={tournament.city}
-              zipCode={tournament.zipCode}
-              numberOfCourts={tournament.numberOfCourts}
-              matchDuration={tournament.matchDuration}
-              breakDuration={tournament.breakDuration}
-              organizerId={tournament.organizerId}
-              createdAt={tournament.createdAt}
-              updatedAt={tournament.updatedAt}
-            />
+            <div key={tournament.id} className="flex justify-center">
+              <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-md">
+                <TournamentCard
+                  id={tournament.id}
+                  name={tournament.name}
+                  startTime={tournament.startTime}
+                  locationName={tournament.locationName}
+                  street={tournament.street}
+                  city={tournament.city}
+                  zipCode={tournament.zipCode}
+                  numberOfCourts={tournament.numberOfCourts}
+                  matchDuration={tournament.matchDuration}
+                  breakDuration={tournament.breakDuration}
+                  organizerId={tournament.organizerId}
+                  createdAt={tournament.createdAt}
+                  updatedAt={tournament.updatedAt}
+                />
+              </div>
+            </div>
           ))}
         </div>
       )}
