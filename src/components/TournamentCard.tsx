d53187d7c77899de064abf6238539f32110047c9
nejc-j <jernej.jakop3@gmail.com>
2024-07-31 23:46:16 +0200

    Fix cards text max_length

--- src/components/TournamentCard.tsx
+++ src/components/TournamentCard.tsx
@@ -5,6 +5,16 @@ import { Tournament } from '@prisma/client';
 
 interface TournamentCardProps extends Tournament {}
 
+// Utility function to truncate each word longer than a specified length
+const truncateWords = (text: string, maxLength: number) => {
+  return text
+    .split(' ')
+    .map((word) =>
+      word.length > maxLength ? `${word.slice(0, maxLength)}...` : word,
+    )
+    .join(' ');
+};
+
 function TournamentCard({
   id,
   name,
@@ -14,12 +24,14 @@ function TournamentCard({
   street,
   zipCode,
 }: TournamentCardProps) {
+  const maxLength = 15; // You can adjust the maximum length as needed
+
   return (
     <Link href={`/tournament/${id}`}>
       <div className="flex h-full border rounded-lg shadow-lg p-3 bg-white w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-md hover:shadow-xl transition-transform duration-200 cursor-pointer transform hover:scale-103">
         <div className="flex-grow">
           <h2 className="text-base sm:text-lg font-semibold mb-3 text-primary overflow-hidden text-ellipsis">
-            {name}
+            {truncateWords(name, maxLength)}
           </h2>
           <div className="flex items-center text-primary mb-0.5">
             <Calendar className="text-tertiary mr-2" size="16" />
@@ -39,7 +51,10 @@ function TournamentCard({
           <div className="flex items-start text-primary">
             <MapPin className="text-tertiary mr-2 flex-none" size="16" />
             <p className="font-semibold text-xs sm:text-sm">
-              {locationName}, {street}, {city}, {zipCode}
+              {truncateWords(locationName, maxLength)},{' '}
+              {truncateWords(street, maxLength)},{' '}
+              {truncateWords(city, maxLength)},{' '}
+              {truncateWords(zipCode, maxLength)}
             </p>
           </div>
         </div>
